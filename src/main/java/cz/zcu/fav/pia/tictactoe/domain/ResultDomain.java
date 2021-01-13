b4514e6f973a74a31a7a54cefbda28d6a288e54f
castic96 <zdenek.castoral.jr@gmail.com>
2021-01-13 15:47:25 +0100

    Přidání data k výsledkům.

--- src/main/java/cz/zcu/fav/pia/tictactoe/domain/ResultDomain.java
+++ src/main/java/cz/zcu/fav/pia/tictactoe/domain/ResultDomain.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 @AllArgsConstructor
@@ -14,5 +15,10 @@ public class ResultDomain {
     private boolean winner;
 
     private LocalDateTime dateTime;
+
+    public String getFormattedDateTime() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd. MM. yyyy HH:mm:ss");
+        return this.dateTime.format(formatter);
+    }
 
 }
